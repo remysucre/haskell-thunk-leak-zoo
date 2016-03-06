4677e68925f01e11d6f6a19b9cffb65b6614d4b2
Remy Wang <remy.sucre@gmail.com>
2016-03-06 12:12:49 -0500

    sumacc

--- sumacc/sumacc.hs
+++ sumacc/sumacc.hs
@@ -5,11 +5,13 @@ ns' = [1..9]
 
 sum' :: [Integer] -> Integer -> Integer -> Integer
 sum' [] n m = n
-sum' (x:xs) !n !m = sum' xs (x + n) (sum'' ns' n)
+sum' (x:xs) !n m = sum' xs (x + n) (sum'' ns' n)
 
-{- the bang before m is better than no bang on its own, but needs to be rid of
+{- the bang before m is better than no bang at all, but needs to be rid of
 when there's a bang before n. In other words, the performance ranking is: 
-best = !n m, middle = n !m, worst = n m
+best = !n m, middle = n !m and !n !m, worst = n m. The reason is, m plugs 
+the thunk leak in n but does extra computation for sum''. The best stratgy 
+only bangs n. 
 -}
 
 sum'' :: [Integer] -> Integer -> Integer
